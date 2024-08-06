69fdf77346c61b18e391755695b9077d88eef370
emilia31 <emiliacristina31@gmail.com>
2024-08-06 17:12:50 +0300

    filter, downsampling,voxel_grid

--- day3.py
+++ day3.py
@@ -2,103 +2,56 @@ import open3d as o3d
 import numpy as np
 
 
-#citirea si vizualizarea datelor PCD initiale
-
-def read_visualise(pcd_name):
-    pcd = o3d.io.read_point_cloud(pcd_name)
-    pcd.paint_uniform_color([0, 0, 1]) #punem toate punctele initiale pe culoarea albastra
-    o3d.visualization.draw_geometries([pcd])
+class PointCloudProcessor:
+    def __init__(self, pcd_name):
+        self.pcd = o3d.io.read_point_cloud(pcd_name)
 
 
-#pcd_name = "files/transit_4.pcd"
-#read_visualise(pcd_name)
+    def visualize(self):
+        o3d.visualization.draw_geometries([self.pcd])
+
+    def filter_outliers(self, nb_neighbors = 20, std_ratio = 2.0, visualize = False):
+        #eliminam punctele care se afla la o distanta prea mare fata de majoritatea punctelor vecine, adica outliers
+        #punctele sunt erori de masuratori
+        #nb_neighbors = numarul de vecini considerati pentru fiecare punct
+        #std_ratio = factor care determina cat de departe poate fi un punct de vecinii sai
+        #punctele care sunt la o distanta mai mare de std_ratio vor fi eliminate
+        #remove_statistical_outiler calculeaza distanta medie si abaterea standard
+
+        _, ind = self.pcd.remove_statistical_outlier(nb_neighbors, std_ratio) #ind este un vector NumPy ce contine indexul punctelor care se incadreaza in distanta
+        self.pcd = self.pcd.select_by_index(ind) #actualizam norul de puncte cu punctele ramase
+        if visualize:
+            self.visualize()
+
+    def downsample_voxel(self, voxel_size=0.01): #reducem numarul de puncte din norul de puncte prin aplicarea voxel gridului
+        self.pcd = self.pcd.voxel_down_sample(voxel_size)
+
+    def visualize_voxel_grid(self, voxel_size=0.05):
+        print('Display voxel grid')
+        voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(self.pcd, voxel_size=voxel_size)
+        o3d.visualization.draw_geometries([voxel_grid])
+
+    def normalize_and_color(self): #normalizam si coloram norul de puncte pentru a-l vizualiza mai usor
+        self.pcd.scale(1 / np.max(self.pcd.get_max_bound() - self.pcd.get_min_bound()), center=self.pcd.get_center())
+        N = len(self.pcd.points)
+        self.pcd.colors = o3d.utility.Vector3dVector(np.random.uniform(0, 1, size=(N, 3)))
+
+    def visualize(self):
+        o3d.visualization.draw_geometries([self.pcd])
 
 
-#preprocesare
-# prin filtrare eliminam punctele izolate. punctele care sunt prea departe de majoritatea punctelor vecine for fi sterse
-def highlight (pcd, nb_neighbors = 20, std_ratio = 2.0):
-    """Filtreaza outliers si evidentiaza """
-    # cl = obiectul ce contine rezultatele filtrarii
-    # ind = vector de indecsi pentru punctele care au fost considerate valide
-    cl, ind = pcd.remove_statistical_outlier(nb_neighbors = nb_neighbors, std_ratio = std_ratio)
-    pcd_filtered = pcd.select_by_index(ind)
-    pcd_outliers = pcd.select_by_index(ind, invert = True)
+def main():
+    pcd_name = "files/transit_4.pcd"
+    processor = PointCloudProcessor(pcd_name)
+    processor.filter_outliers(visualize=True)
+    processor.downsample_voxel(voxel_size=0.1)
+    processor.normalize_and_color()
 
-    #coloram punctele
-    pcd_filtered.paint_uniform_color([0.0, 1.0, 0.0]) #verde pentru punctele valabile
-    pcd_outliers.paint_uniform_color([1.0, 0.0, 0.0]) #rosu
-
- #   o3d.visualization.draw_geometries([pcd_filtered, pcd_outliers])
-
-    return pcd_filtered, pcd_outliers
-
-#pcd_name = "files/transit_4.pcd"
-#pcd = o3d.io.read_point_cloud(pcd_name)
-#pcd_filtered, pcd_outliers = highlight(pcd)
-
-
-def filter (pcd, nb_neighbors = 20, std_ratio = 2.0):
-    cl, ind = pcd.remove_statistical_outlier(nb_neighbors = nb_neighbors, std_ratio = std_ratio)
-    pcd_filtered = pcd.select_by_index(ind)
-
-    pcd_filtered.paint_uniform_color([0.0, 1.0, 0.0]) #verde pentru punctele valabile
-
-    o3d.visualization.draw_geometries([pcd_filtered])
-
-    return pcd_filtered
-
-#pcd_name = "files/transit_4.pcd"
-#pcd = o3d.io.read_point_cloud(pcd_name)
-#pcd_filtered = filter(pcd)
-
-
-
-#downsampling ==> reducem numarul de puncte din nor prin preluarea unui subset
-#voxel downsampling
-
-def downsample_voxel(pcd, voxel_size=0.1):
-
-    print(f"Downsampling cu dimensiunea voxelului de {voxel_size:.3f} metri...")
-    pcd_downsampled = pcd.voxel_down_sample(voxel_size)
-    print(f"Numarul de puncte inainte de downsampling: {len(pcd.points)}")
-    print(f"Numarul de puncte dupa downsampling: {len(pcd_downsampled.points)}")
-
-    return pcd_downsampled
-
-#pipeline principal
-#pcd = o3d.io.read_point_cloud(pcd_name)
-#pcd_filtered = filter(pcd)
-#pcd_downsampled = downsample_voxel(pcd_filtered, voxel_size= 0.1)
-
-def slice_point_cloud(pcd, y_value, thickness = 0.01):
-    """Feliem norul de puncte utilizand segment_plane"""
-    plane_model = [0, 1, 0, -y_value]
-    #segmentarea norului de puncte
-    _, inliers = pcd.segment_plane(distance_threshold=thickness, ransac_n=3, num_iterations=1000)
-    sliced_pcd = pcd.select_by_index(inliers)
-    return sliced_pcd
+    print('Displaying input point cloud: ')
+    processor.visualize()
+    processor.visualize_voxel_grid(voxel_size=0.05)
 
 
 
-
-
-pcd_name = "files/transit_4.pcd"
-pcd = o3d.io.read_point_cloud(pcd_name)
-pcd_filtered_1, pcd_outliers = highlight(pcd)
-pcd_filtered_2 = filter(pcd)
-pcd_downsampled = downsample_voxel(pcd_filtered_2, voxel_size= 0.1)
-
-nr_slices = 10
-y_min, y_max = pcd_downsampled.get_min_bound()[1], pcd_downsampled.get_max_bound()[1]
-y_interval = (y_max - y_min)/nr_slices
-
-all_slices = []
-for i in range(nr_slices):
-    y_value = y_min + i * y_interval
-    slice = slice_point_cloud(pcd_downsampled, y_value)
-    slice.paint_uniform_color([1.0, 0.0, 0.0])
-    all_slices.append(slice)
-
-o3d.visualization.draw_geometries(all_slices + [pcd_outliers])
-
-
+if __name__ == "__main__":
+    main()
