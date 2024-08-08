d6cd3e6cd695610a31c58b8dc913999b7ed309c2
gabarit_open3D <emiliacristina31@gmail.com>
2024-08-08 13:58:23 +0300

    eliminare puncte considerate picaturi de ploaie; am adaugat vizualizare de distributii pentru a regla distanta fata de vecini

--- day3.py
+++ day3.py
@@ -1,6 +1,7 @@
 import os.path
 import open3d as o3d
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class PointCloudProcessor:
@@ -12,7 +13,7 @@ class PointCloudProcessor:
 
     def filter_outliers(self, nb_neighbors=20, std_ratio=2.0, visualize=False):
         #eliminam punctele care se afla la o distanta prea mare fata de majoritatea punctelor vecine, adica outliers
-        #punctele sunt erori de masuratori
+        #punctele sunt erori de masuratori, nu fac parte din obiect
         #nb_neighbors = numarul de vecini considerati pentru fiecare punct
         #std_ratio = factor care determina cat de departe poate fi un punct de vecinii sai
         #punctele care sunt la o distanta mai mare de std_ratio vor fi eliminate
@@ -45,6 +46,59 @@ class PointCloudProcessor:
             o3d.visualization.draw_geometries([pcd])
         else:
             o3d.visualization.draw_geometries([self.pcd])
+
+    def rain_drop_remove(self, pcd, search_radius = 0.3, max_nn = 90, std_dev_multiplier = 1.0):
+    #functie pentru eliminarea punctelor care sunt considerate picaturi de ploaie
+    #search_radius = raza de cautare in jurul fiecarui punct pentru a gasi vecinii
+    #max_nn = numarul maxim de vecini
+    #std_dev_multiplier = controleaza variatia distantei fata de vecini a.i. sa fie considerat o picatura de ploaie
+
+        points = np.asarray(pcd.points)
+        pcd_tree = o3d.geometry.KDTreeFlann(pcd) #cream un arbore folosit la cautarea vecinilor
+
+        removing = []
+        mean_dists = []
+        std_devs = []
+
+        for i, point in enumerate(points):
+            [k, idx, _] = pcd_tree.search_radius_vector_3d(point, search_radius) #cauta cei mai apropiati max_nn vecini in raza data
+            if k < 3: #ocolim punctele cu mai putin de 3 vecini
+                continue
+
+            neighbors = points[idx, :] #coordonatele vecinului
+
+            distances = np.linalg.norm(neighbors - point, axis = 1) #lungimea ca diferenta dintre punctul curent si vecinul sau
+
+            mean_dist = np.mean(distances)
+            std_dev = np.std(distances)
+
+            mean_dists.append(mean_dist)
+            std_devs.append(std_dev)
+
+            #verificam daca punctul este picatura de ploaie:
+            threshold = std_dev_multiplier * mean_dist
+            if std_dev > threshold:
+                removing.append(i)
+
+        #distributia distantelor pentru a ajusta pragurile
+        plt.figure(figsize=(12, 6))
+        plt.subplot(1, 2, 1)
+        plt.hist(mean_dists, bins=50, color='blue', alpha=0.7)
+        plt.title('Distribuția distanțelor medii')
+        plt.xlabel('Distanta medie')
+        plt.ylabel('Frecvența')
+
+        plt.subplot(1, 2, 2)
+        plt.hist(std_devs, bins=50, color='red', alpha=0.7)
+        plt.title('Distributia deviatiilor standard')
+        plt.xlabel('Deviatia standard')
+        plt.ylabel('Frecventa')
+
+        plt.tight_layout()
+        plt.show()
+
+        without_rain_pcd = pcd.select_by_index(removing, invert = True)
+        return without_rain_pcd
 
     def slice_point_cloud(self, num_slices, axis, output="slices"):
 
@@ -114,7 +168,15 @@ def main():
     pcd_name = "files/transit_4.pcd"
     output = "slices"
     processor = PointCloudProcessor(pcd_name)
+
+    print(f"Numarul de puncte initial: {len(processor.pcd.points)}")
+    #eliminam picaturile de ploaie:
+    processor.pcd = processor.rain_drop_remove(processor.pcd)
+    print(f"Numarul de puncte dupa rain_drop_remove: {len(processor.pcd.points)}")
+
+    #filtram zgomotul:
     processor.filter_outliers(visualize=True)
+
     processor.downsample_voxel(voxel_size=0.1)
     processor.normalize_and_color(processor.pcd)
 
@@ -122,6 +184,8 @@ def main():
     processor.visualize_voxel_grid(voxel_size=0.05)
     processor.visualize()
 
+
+    #feliere:
     num_slices = 20
     axis = 2 #alegem axa Z pentru a felia norul de puncte
     file_paths = processor.slice_point_cloud(num_slices, axis, output)
