04b8987e2518a18c17323ffea47683c69114783f
gabarit_open3D <emiliacristina31@gmail.com>
2024-08-13 12:43:38 +0300

    modificari: - am renuntat la functia de normalizare (nu ajuta in obtinerea rezulatelor favorabile) - am modificat functia slice_point_cloud, acum feliile au dimensiuni apropiate - am modificat functia calculate_slice_dimensions (simplificat), functia calculeaza din start latimea si inaltimea - inca nu am apelat functia detect si visualize oversize(urmeaza) - in main am micsorat dimensiunea voxelului (de la 0.1 la 0.05), calculam dimensiunile initiale si comparam cu rezultatele obtinute. - latimea este comparata cu cea initiala, iar inaltimea(pentru ca feliem pe axa Z) o adunam pe fiecare felie si comparam cu cea initiala

--- day3.py
+++ day3.py
@@ -1,4 +1,3 @@
-import os.path
 import open3d as o3d
 import numpy as np
 from matplotlib import pyplot as plt
@@ -9,8 +8,13 @@ class PointCloudProcessor:
     def __init__(self, pcd_name):
         self.pcd = o3d.io.read_point_cloud(pcd_name)
 
-    def visualize(self):
-        o3d.visualization.draw_geometries([self.pcd])
+    def visualize(self, points=None):
+        if points is not None:
+            pcd = o3d.geometry.PointCloud()
+            pcd.points = o3d.utility.Vector3dVector(points)
+            o3d.visualization.draw_geometries([pcd])
+        else:
+            o3d.visualization.draw_geometries([self.pcd])
 
     def filter_outliers(self, nb_neighbors=20, std_ratio=2.0, visualize=False):
         #eliminam punctele care se afla la o distanta prea mare fata de majoritatea punctelor vecine, adica outliers
@@ -20,55 +24,45 @@ class PointCloudProcessor:
         #punctele care sunt la o distanta mai mare de std_ratio vor fi eliminate
         #remove_statistical_outiler calculeaza distanta medie si abaterea standard
 
-        _, ind = self.pcd.remove_statistical_outlier(nb_neighbors,
-                                                     std_ratio)  #ind este un vector NumPy ce contine indexul punctelor care se incadreaza in distanta
-        self.pcd = self.pcd.select_by_index(ind)  #actualizam norul de puncte cu punctele ramase
+        # ind este un vector NumPy ce contine indexul punctelor care se incadreaza in distanta
+        _, ind = self.pcd.remove_statistical_outlier(nb_neighbors, std_ratio)
+        # actualizam norul de puncte cu punctele ramase
+        self.pcd = self.pcd.select_by_index(ind)
         if visualize:
             self.visualize()
 
-    def downsample_voxel(self,
-                         voxel_size=0.01):  #reducem numarul de puncte din norul de puncte prin aplicarea voxel gridului
+    # reducem numarul de puncte din norul de puncte prin aplicarea voxel gridului
+    def downsample_voxel(self, voxel_size=0.01):
         self.pcd = self.pcd.voxel_down_sample(voxel_size)
 
-    def visualize_voxel_grid(self, voxel_size=0.05):
-        print('Display voxel grid')
-        voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(self.pcd, voxel_size=voxel_size)
-        o3d.visualization.draw_geometries([voxel_grid])
 
-    def normalize_and_color(self, pcd):  #normalizam si coloram norul de puncte pentru a-l vizualiza mai usor
-        self.pcd.scale(1 / np.max(self.pcd.get_max_bound() - self.pcd.get_min_bound()), center=self.pcd.get_center())
-        N = len(self.pcd.points)
-        self.pcd.colors = o3d.utility.Vector3dVector(np.random.uniform(0, 1, size=(N, 3)))
 
-    def visualize(self, points=None):
-        if points is not None:
-            pcd = o3d.geometry.PointCloud()
-            pcd.points = o3d.utility.Vector3dVector(points)
-            o3d.visualization.draw_geometries([pcd])
-        else:
-            o3d.visualization.draw_geometries([self.pcd])
+    @staticmethod
+    def rain_drop_remove(pcd, search_radius=0.3, max_nn=90, std_dev_multiplier=1.0):
+        #functie pentru eliminarea punctelor care sunt considerate picaturi de ploaie
+        #search_radius = raza de cautare in jurul fiecarui punct pentru a gasi vecinii
+        #max_nn = numarul maxim de vecini
+        #std_dev_multiplier = controleaza variatia distantei fata de vecini a.i. sa fie considerat o picatura de ploaie
 
-    def rain_drop_remove(self, pcd, search_radius = 0.3, max_nn = 90, std_dev_multiplier = 1.0):
-    #functie pentru eliminarea punctelor care sunt considerate picaturi de ploaie
-    #search_radius = raza de cautare in jurul fiecarui punct pentru a gasi vecinii
-    #max_nn = numarul maxim de vecini
-    #std_dev_multiplier = controleaza variatia distantei fata de vecini a.i. sa fie considerat o picatura de ploaie
-
+        # cream un arbore folosit la cautarea vecinilor
         points = np.asarray(pcd.points)
-        pcd_tree = o3d.geometry.KDTreeFlann(pcd) #cream un arbore folosit la cautarea vecinilor
+        pcd_tree = o3d.geometry.KDTreeFlann(pcd)
 
         removing = []
         mean_dists = []
         std_devs = []
 
         for i, point in enumerate(points):
-            [k, idx, _] = pcd_tree.search_radius_vector_3d(point, search_radius) #cauta cei mai apropiati max_nn vecini in raza data
-            if k < 3: #ocolim punctele cu mai putin de 3 vecini
+            # cauta cei mai apropiati max_nn vecini in raza data
+            [k, idx, _] = pcd_tree.search_radius_vector_3d(point, search_radius)
+            # ocolim punctele cu mai putin de 3 vecini
+            if k < 3:
                 continue
 
-            neighbors = points[idx, :] #coordonatele vecinului
+            neighbors = points[idx, :]
 
-            distances = np.linalg.norm(neighbors - point, axis = 1) #lungimea ca diferenta dintre punctul curent si vecinul sau
+            # lungimea ca diferenta dintre punctul curent si vecinul sau
+            distances = np.linalg.norm(neighbors - point, axis=1)
 
             mean_dist = np.mean(distances)
             std_dev = np.std(distances)
@@ -98,44 +92,39 @@ class PointCloudProcessor:
         plt.tight_layout()
         plt.show()
 
-        without_rain_pcd = pcd.select_by_index(removing, invert = True)
+        without_rain_pcd = pcd.select_by_index(removing, invert=True)
         return without_rain_pcd
 
-    def slice_point_cloud(self, num_slices, axis, output="slices"):
+    def slice_point_cloud(self, num_slices, axis, output_dir):
+        points_np = np.asarray(self.pcd.points)
 
-        points = np.asarray(self.pcd.points)  #extrage coordonatele x, y si z ale punctelor si le converteste intr-un array numpy pt a permite operatii
-        min_val = np.min(points[:, axis])  #valoarea minima
-        max_val = np.max(points[:, axis])  #valoarea maxima
+        min_val = points_np[:, axis].min()
+        max_val = points_np[:, axis].max()
+        total_depth = max_val - min_val
 
-        slice_width = (max_val - min_val) / num_slices  #latimea fiecarei slice
+        slice_depth = total_depth / num_slices
+        slice_files = []
 
-        os.makedirs(output, exist_ok=True)  #creeaza directorul pentru continutul fiecarei felii
-        file_paths = []  #lista care stocheaza caile catre fisiere
-
-        slice_counter = 1
         for i in range(num_slices):
+            lower_bound = min_val + i * slice_depth
+            upper_bound = lower_bound + slice_depth
 
-            #excludem prima si ultima felie (pentru a elimina partea nesemnificativa)
-            if i == 0 or i == num_slices - 1:
-                continue
+            #filtram punctele care se află în intervalul [lower_bound, upper_bound] pe axa specificată
+            mask = (points_np[:, axis] >= lower_bound) & (points_np[:, axis] < upper_bound)
+            slice_points = points_np[mask]
 
-            start_val = min_val + i * slice_width  #valoarea de start a coordonatei pe axa pentru felia i
-            end_val = start_val + slice_width
-            mask = (points[:, axis] >= start_val) & (
-                        points[:, axis] <= end_val)  #masca pentru a identifica punctele aflate in interval
-            sliced_points = points[mask]  #extragem punctele ce apartin feliei i
+            #cream un nou nor de puncte pentru felie
+            slice_pcd = o3d.geometry.PointCloud()
+            slice_pcd.points = o3d.utility.Vector3dVector(slice_points)
 
-            #cream un nou obiect pt a stoca punctele feliei
-            new_pcd = o3d.geometry.PointCloud()
-            new_pcd.points = o3d.utility.Vector3dVector(sliced_points)
-            self.normalize_and_color(new_pcd)
-            file_path = os.path.join(output, f"slice_{i + 1}.pcd")
-            o3d.io.write_point_cloud(file_path, new_pcd)
-            file_paths.append(file_path)
+            #salveaza felia
+            slice_filename = f"{output_dir}/slice_{i + 1}.pcd"
+            o3d.io.write_point_cloud(slice_filename, slice_pcd)
+            slice_files.append(slice_filename)
 
-            slice_counter += 1
+        return slice_files
 
-        return file_paths
+
 
     def calculate_slice_dimensions(self, file_paths):
         slice_dimensions = []  #lista pentru a stoca dimensiunile
@@ -143,28 +132,18 @@ class PointCloudProcessor:
             pcd_slice = o3d.io.read_point_cloud(file_path)
             points_np = np.asarray(pcd_slice.points)
 
-            #cautam indecsii punctelor extreme cu argmin si argmax
-            left_idx = np.argmin(points_np[:, 0]) #axa X
-            right_idx = np.argmax(points_np[:, 0]) #axa X
-            down_idx = np.argmin(points_np[:, 2]) #axa Z
-            up_idx = np.argmax(points_np[:, 2]) #axa Z
 
-            #scoatem punctele extreme
-            left_point = pcd_slice.points[left_idx]
-            right_point = pcd_slice.points[right_idx]
-            down_point = pcd_slice.points[down_idx]
-            up_point = pcd_slice.points[up_idx]
+            x_min, y_min, z_min = points_np.min(axis=0)
+            x_max, y_max, z_max = points_np.max(axis=0)
 
-            #calcul dimensiuni, ne folosim de distanta euclidiana
-            width = np.linalg.norm(np.asarray(right_point) - np.asarray(left_point))
-            height = np.linalg.norm(np.asarray(up_point) - np.asarray(down_point))
-            depth = np.max(points_np[:, 1]) - np.min(points_np[:, 1])
+            width = x_max - x_min
+            height = z_max - z_min
 
-            slice_dimensions.append({"width": width, "height": height, "depth": depth})
+            slice_dimensions.append({"width": width, "height": height})
 
         return slice_dimensions
 
-    def detect_oversize (self, pcd_slice, threshold, axis):
+    def detect_oversize(self, pcd_slice, threshold, axis):
         #pcd_slice = felia de nor de puncte
         #threshold = pragul
 
@@ -183,15 +162,16 @@ class PointCloudProcessor:
         #identificare depasiri
         inlier_mask = errors < threshold
         outlier_mask = np.logical_not(inlier_mask)
-        oversize_indices= np.where(outlier_mask)[0]
+        oversize_indices = np.where(outlier_mask)[0]
 
         return oversize_indices
+
     def visualize_oversizes(self, pcd_slice, oversize_ind_width, oversize_ind_height):
         color_pcd = o3d.geometry.PointCloud()
         color_pcd.points = pcd_slice.points
         colors = np.asarray(color_pcd.colors)
 
-        colors[oversize_ind_width] = [1, 0, 0] #rosu pentru depasiri de latime
+        colors[oversize_ind_width] = [1, 0, 0]  #rosu pentru depasiri de latime
         colors[oversize_ind_height] = [0, 0, 1]  # albastru pentru depasiri de latime
         color_pcd.colors = o3d.utility.Vector3dVector(colors)
 
@@ -199,9 +179,10 @@ class PointCloudProcessor:
 
 
 def main():
-    pcd_name = "files/transit_4.pcd"
+    pcd_name = "files/604-semitruck_tanker-00148.pcd"
     output = "slices"
     processor = PointCloudProcessor(pcd_name)
+    processor.visualize()
 
     print(f"Numarul de puncte initial: {len(processor.pcd.points)}")
     #eliminam picaturile de ploaie:
@@ -212,46 +193,47 @@ def main():
     processor.filter_outliers(visualize=True)
     print(f"Numarul de puncte dupa filter_outliers: {len(processor.pcd.points)}")
 
-    processor.downsample_voxel(voxel_size=0.1)
-    processor.normalize_and_color(processor.pcd)
-    print(f"Numarul de puncte dupa voxel: {len(processor.pcd.points)}")
+    processor.downsample_voxel(voxel_size=0.05)
+    print(f"Numarul de puncte dupa voxel: {len(processor.pcd.points)}\n")
 
-    print('Displaying input point cloud: ')
-    processor.visualize_voxel_grid(voxel_size=0.05)
     processor.visualize()
 
+    points_np = np.asarray(processor.pcd.points)
+    x_min = points_np[:, 0].min()
+    x_max = points_np[:, 0].max()
 
-    #feliere:
-    num_slices = 20
-    axis = 2 #alegem axa Z pentru a felia norul de puncte
-    file_paths = processor.slice_point_cloud(num_slices, axis, output)
-    #pragul stabilit:
+    z_min = points_np[:, 2].min()
+    z_max = points_np[:, 2].max()
+
+    width = x_max - x_min
+    height = z_max - z_min
+
+
+    num_slices = 5
+    axis = 2  #alegem axa Z pentru a felia norul de puncte
+    file_paths = processor.slice_point_cloud( num_slices, axis, output)
+
     width_limit = 2.55
     height_limit = 4.0
 
     slice_dim = processor.calculate_slice_dimensions(file_paths)
     for slice_idx, file_path in enumerate(file_paths):
-        print(f"Felia din: {file_path}")
         pcd_slice = o3d.io.read_point_cloud(file_path)
         processor.visualize(np.asarray(pcd_slice.points))
-
         dimensions = slice_dim[slice_idx]
-        print(f"Slice {slice_idx + 1}: ")
-        print(f"Width: {dimensions['width']:.2f}")
-        print(f"Height: {dimensions['width']:.2f}")
-        print(f"Depth: {dimensions['depth']:.2f}")
-
-        oversize_ind_width = processor.detect_oversize(pcd_slice, width_limit/2, axis = 0)
-        oversize_ind_height = processor.detect_oversize(pcd_slice, height_limit / 2, axis=2)
-
-        print(f"Vizualizare depasire width: {oversize_ind_width} and height {oversize_ind_height}")
-        processor.visualize_oversizes(pcd_slice, oversize_ind_width, oversize_ind_height)
 
 
 
 
+    consistent_width = all(np.isclose(slice_dim[i]["width"], width) for i in range(num_slices))
+    print(f"Latimea este constanta pe toate feliile: {consistent_width}, latimea initiala este: {width}")
+
+    for i in range(num_slices):
+        print(f"Felia {i + 1} are latimea: {slice_dim[i]['width']:.2f} si inaltimea: {slice_dim[i]['height']:.2f}")
 
 
+    total_height = sum(slice_dim[i]["height"] for i in range(num_slices))
+    print(f"Suma inaltimilor feliilor: {total_height:.2f} (inaltimea originala: {height:.2f})")
 
 
 if __name__ == "__main__":
